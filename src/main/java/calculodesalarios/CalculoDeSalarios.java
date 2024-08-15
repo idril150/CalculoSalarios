3be5b4faca234fd55215a58bd58774ec9d6916cb
idril150 <ebibari1@gmail.com>
2024-08-15 15:33:28 -0600

    creacion del modelo de usuarios, modelo de pago, y sus controladores de datos

--- src/main/java/calculodesalarios/CalculoDeSalarios.java
+++ src/main/java/calculodesalarios/CalculoDeSalarios.java
@@ -4,6 +4,12 @@
 
 package calculodesalarios;
 
+import controller.EmpleadoJpaController;
+import controller.PagoJpaController;
+import java.util.List;
+import model.Empleado;
+import model.Pago;
+
 /**
  *
  * @author Soporte
@@ -12,5 +18,28 @@ public class CalculoDeSalarios {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        EmpleadoJpaController ectrl = new EmpleadoJpaController();
+        PagoJpaController pctrl = new PagoJpaController();
+        /*Empleado empleado = new Empleado(2, "pedro", "oficina", 8600, "tuxtla");
+        
+        ctrl.create(empleado);*/
+        Pago pag = new Pago(1 ,100.0 , 200.0, 100.0, 2600.0, 2700.0, 20240815);
+        
+        pctrl.create(pag);
+        /*List<Pago> pagos = ctrl.findPagos(1);
+        for(Pago pago : pagos){
+            System.out.println(pago);
+        }*/
+        //Proc*esosController pCtrl = new ProcesosController();v
+        //pCtrl.copiarEmpleados();
+        List<Empleado> lista = ectrl.findEmpleados();
+        for(Empleado empleado : lista){
+            System.out.print("al empleado "+empleado.getNombre()+"");
+            List<Pago> pagos = pctrl.findPagos(empleado.getId());
+            for(Pago pago : pagos){
+                System.out.print("se le pago "+pago.getPagtot()+" la fecha de "+pago.getFech()+"\n");
+            }
+            System.out.println("\n\n");
+        }
     }
 }
